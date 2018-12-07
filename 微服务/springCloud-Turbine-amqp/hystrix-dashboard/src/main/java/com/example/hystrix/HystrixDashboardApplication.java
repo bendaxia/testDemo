7e4b222c713810820dcfc16fdcc20046d54ccb5e
渣渣码农 <916917649@qq.com>
2018-12-07 12:31:00 +0800

    通过消息代理方式进行Hystrix监控数据聚合

---
+++ 微服务/springCloud-Turbine-amqp/hystrix-dashboard/src/main/java/com/example/hystrix/HystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+@EnableHystrixDashboard  //启用Hystrix Dashboard功能
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDashboardApplication.class, args);
+	}
+}
