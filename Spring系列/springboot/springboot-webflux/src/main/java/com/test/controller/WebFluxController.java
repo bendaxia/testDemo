f38958f53db05948ba396b06f56830abaeacdf35
贲国龙 <benguolong@yhfin.com>
2019-03-12 13:33:42 +0800

    一款异步非阻塞能够在有限的资源下提高系统吞吐量和伸缩性的响应式web框架,支持函数式编程

--- Spring系列/springboot/springboot-webflux/src/main/java/com/test/controller/WebFluxController.java
+++ Spring系列/springboot/springboot-webflux/src/main/java/com/test/controller/WebFluxController.java
@@ -1,4 +1,4 @@
-package com.test.controller;
+﻿package com.test.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,21 +14,5 @@ public class WebFluxController {
 	@GetMapping("/test")
 	public String test() {
 		return "hello SpringWebFlux!";
-	}
-	
-	//引用对象不会创建内存。
-	//int[] price; 数组变量
-	//int[] price = new int{1,2,3}; 数组对象
-	public static void main(String[] args) {
-		String [] a = {"1","2"};
-		String [] b = a;
-		String [] c = a;
-		System.out.println(b[0]+","+b[1]);
-		System.out.println(c[0]+","+c[1]);
-		System.out.println("-------------");
-		b[0] = "8";
-		System.out.println(b[0]+","+b[1]);
-		System.out.println(c[0]+","+c[1]);
-		System.out.println(a[0]+","+a[1]);
 	}
 }
